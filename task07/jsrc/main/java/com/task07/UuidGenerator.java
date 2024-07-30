10daf961fde8b84100a36fd2a27541829403a45f
mahi-2307 <mahireddy.katam@gmail.com>
2024-07-30 23:05:40 +0530

    added the cloudwatch trigger

--- task07/jsrc/main/java/com/task07/UuidGenerator.java
+++ task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -3,6 +3,8 @@ package com.task07;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -21,6 +23,11 @@ import java.time.format.DateTimeFormatter;
 		roleName = "uuid_generator-role",
 		isPublishVersion = false,
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+
+@DependsOn(
+		name = "uuid_trigger",
+		resourceType = ResourceType.CLOUDWATCH_RULE
 )
 public class UuidGenerator implements RequestHandler<ScheduledEvent, Map<String, Object>> {
 
