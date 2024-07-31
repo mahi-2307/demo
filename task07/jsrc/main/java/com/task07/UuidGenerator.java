7059f00d06620655148c0e637e09382b91343a35
mahi-2307 <mahireddy.katam@gmail.com>
2024-07-31 10:33:07 +0530

    added the cloudwatch trigger

--- task07/jsrc/main/java/com/task07/UuidGenerator.java
+++ task07/jsrc/main/java/com/task07/UuidGenerator.java
@@ -2,24 +2,25 @@ package com.task07;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.syndicate.deployment.annotations.events.RuleEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.ArrayList;
-import java.util.List;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 @LambdaHandler(lambdaName = "uuid_generator",
 		roleName = "uuid_generator-role",
@@ -39,9 +40,8 @@ public class UuidGenerator implements RequestHandler<ScheduledEvent, Map<String,
 
 	@Override
 	public Map<String, Object> handleRequest(ScheduledEvent event, Context context) {
-		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
-		String timestamp = Instant.now().toString();
-		String fileName = timestamp + ".json";
+		AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard().build();
+		String key = Instant.now().toString() + ".json";
 
 		List<String> uuids = new ArrayList<>();
 		for (int i = 0; i < 10; i++) {
@@ -49,8 +49,20 @@ public class UuidGenerator implements RequestHandler<ScheduledEvent, Map<String,
 		}
 
 		String content = "{\n  \"ids\": [\n    \"" + String.join("\",\n    \"", uuids) + "\"\n  ]\n}";
+		File file = new File("/tmp/AWS.txt");
 
-		s3Client.putObject(BUCKET_NAME, fileName, content);
+		try (FileWriter writer = new FileWriter(file)) {
+			writer.write(content);
+		} catch (IOException e) {
+			e.printStackTrace();
+			Map<String, Object> errorResult = new HashMap<>();
+			errorResult.put("statusCode", 500);
+			errorResult.put("body", "Error writing to file: " + e.getMessage());
+			return errorResult;
+		}
+
+		amazonS3.putObject(new PutObjectRequest(BUCKET_NAME, key, file));
+
 		Map<String, Object> resultMap = new HashMap<>();
 		resultMap.put("statusCode", 200);
 		resultMap.put("body", "UUIDs generated and stored in S3 bucket");
